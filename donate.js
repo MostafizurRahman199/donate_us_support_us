38b1c7b175b347d74dbaa67de719691355b4cea7
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-09-23 02:56:45 +0600

    navbar fixed made responsive

--- donate.js
+++ donate.js
@@ -5,13 +5,25 @@ const quotaInput = getById("quota_amount");
 const noakhaliBtn = getById("noakhali_donate_btn");
 const feniBtn = getById("feni_donate_btn");
 const quotaBtn = getById("quota_donate_btn");
+
+const donate_btn = getById("donate_btn");
+const donate_btn_2 = getById("donate_btn_2");
+const history_btn = getById("history_btn");
+const history_btn_2 = getById("history_btn_2");
+const blog_btn = getById("blog_btn");
+
 const my_modal_1 = getById("my_modal_1");
 const balance = getById("balance");
 const modal_close = getById("modal_close");
+const history_section = getById("history_section");
+const card_section = getById("card_section");
+const footer = getById("footer");
 
 const quota_balance= getById("quota_balance");
 const feni_balance = getById("feni_balance");
 const noakhali_balance = getById("noakhali_balance");
+
+
 
 
 
@@ -44,7 +56,6 @@ function reset(){
 
 
 function HistoryAdd(amount, text){
-    const history_section = getById("history_section");
     const div = document.createElement("div");
     const date = new Date();
     const formattedDate = date.toString();
@@ -77,6 +88,43 @@ function updateDonateBalance(id, amount){
     const total = elementParseValue + newAmount
     element.innerText = total;
 }
+
+
+blog_btn.addEventListener("click", (event)=>{
+    window.location.href = "./blog.html"
+    
+})
+
+donate_btn.addEventListener("click", ()=>{
+    history_section.className = "hidden";
+    card_section.className = "";
+
+
+    history_btn_2.className = "relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600  text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+    history_btn.className = "relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-white-900 text-black hover:text-white rounded-md group-hover:bg-opacity-0"
+    
+    donate_btn_2.className ="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600  text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800";
+
+    donate_btn.className = "relative px-5 py-2.5 transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0";
+
+    footer.className = "";
+})
+history_btn.addEventListener("click", ()=>{
+    card_section.className = "hidden"
+    history_section.className = "flex flex-col space-y-4";
+
+    donate_btn_2.className = "relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600  text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+    donate_btn.className = "relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-white-900 text-black hover:text-white rounded-md group-hover:bg-opacity-0"
+    
+    history_btn_2.className ="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600  text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800";
+
+    history_btn.className = "relative px-5 py-2.5 transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0";
+
+    footer.className = "fixed bottom-0 left-0 w-full";
+
+    
+
+})
 
 
 
